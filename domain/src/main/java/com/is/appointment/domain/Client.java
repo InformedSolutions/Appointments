3b84f33ab46882bc1e1bcf66aaa800b547025cc4
lauren-workman <lauren.workman@informed.com>
2020-07-01 11:41:51 +0100

    Initial classes

---
+++ domain/src/main/java/com/is/appointment/domain/Client.java
@@ -0,0 +1,72 @@
+package com.is.appointment.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    private int id;
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private String emailAddress;
+
+    public Client() {}
+
+    public Client(int id, String name, String address, String phoneNumber, String emailAddress) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Client [id=%s, name=%s, address=%s, phoneNumber=%s, emailAddress=%s]", id, name, address, phoneNumber, emailAddress);
+    }
+}
