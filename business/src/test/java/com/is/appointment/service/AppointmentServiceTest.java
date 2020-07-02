05302c4ade6224cd7eb6dab038a9e900cdc68663
Zachary Wormleighton <zachary.wormleighton@informed.com>
2020-07-02 12:12:49 +0100

    Added extra tests for searches by client/provider

--- business/src/test/java/com/is/appointment/service/AppointmentServiceTest.java
+++ business/src/test/java/com/is/appointment/service/AppointmentServiceTest.java
@@ -16,9 +16,69 @@ public class AppointmentServiceTest {
     @Autowired
     private AppointmentService appointment;
 
+
     @Test
     public void testAppointmentGetAllAppointments() {
         List<Appointment> appointments = appointment.getAllAppointments();
         assertTrue(appointments.size() > 0, "Should be 1 or more appointments returned");
+    }
+
+    @Test
+    public void testAppointmentGetAppointmentsByProvider() {
+        String id = "1";
+        List<Appointment> listAppointment = appointment.getAppointmentsByProvider(id);
+        // Check non-empty list
+        assertTrue(listAppointment.size() > 0, "List appointment should not be empty");
+        // Check list matches
+        boolean result = AppointmentServiceTestUtility.matchIdList(id, "provider", listAppointment);
+        assertTrue(result, "ID list matches " + id);
+    }
+
+    @Test
+    public void testAppointmentGetAppointmentsByClient() {
+        String id = "1";
+        List<Appointment> listAppointment = appointment.getAppointmentsByClient(id);
+        // Check non-empty list
+        assertTrue(listAppointment.size() > 0, "List appointment should not be empty");
+        // Check list matches
+        boolean result = AppointmentServiceTestUtility.matchIdList(id, "client", listAppointment);
+        assertTrue(result, "ID list matches " + id);
+    }
+}
+
+class AppointmentServiceTestUtility {
+
+    static boolean matchIdList(String id, String idType, List<Appointment> listAppointment) {
+        boolean result = true;
+        int numberOfIds = listAppointment.size();
+        switch(idType) {
+            case "provider":
+                List<String> providerIds = listAppointment.stream().map(Appointment::getProvider).collect(Collectors.toList());
+                if (providerIds.size() != numberOfIds) {
+                    result = false;
+                } else {
+                    for (String providerId : providerIds) {
+                        if (!providerId.equals(id)) {
+                            result = false;
+                        }
+                    }
+                }
+                break;
+            case "client":
+                List<String> clientIds = listAppointment.stream().map(Appointment::getClient).collect(Collectors.toList());
+                if (clientIds.size() != numberOfIds) {
+                    result = false;
+                } else {
+                    for (String clientId : clientIds) {
+                        if (!clientId.equals(id)) {
+                            result = false;
+                        }
+                    }
+                }
+                break;
+        }
+
+        return result;
+
     }
 }
